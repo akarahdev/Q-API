f0c06c10d65978fc94a5bf93d15c7c723f9a3afb
akarahdev <endisticakarah@gmail.com>
2024-09-18 10:08:53 -0400

    Add missing components

--- src/main/java/dev/akarah/entities/EntityComponent.java
+++ src/main/java/dev/akarah/entities/EntityComponent.java
@@ -2,6 +2,8 @@ package dev.akarah.entities;
 
 import dev.akarah.component.ComponentMaster;
 
+import java.util.UUID;
+
 public class EntityComponent implements ComponentMaster<Entity, EntityComponent> {
     public static LocationComponent LOCATION =
         new LocationComponent(null, null);
@@ -9,6 +11,40 @@ public class EntityComponent implements ComponentMaster<Entity, EntityComponent>
         new HealthComponent(0, 0);
     public static EquipmentComponent EQUIPMENT =
         new EquipmentComponent(null, null, null, null, null, null);
+
+    public static PlayerComponent PLAYER = new PlayerComponent() {
+        @Override
+        public GUIComponent gui() {
+            return null;
+        }
+
+        @Override
+        public InventoryComponent inventory() {
+            return null;
+        }
+
+        @Override
+        public boolean isPresent(Entity holder) {
+            return false;
+        }
+    };
+
+    public static IdentityComponent IDENTITY = new IdentityComponent() {
+        @Override
+        public int temporaryId() {
+            return 0;
+        }
+
+        @Override
+        public UUID uuid() {
+            return null;
+        }
+
+        @Override
+        public boolean isPresent(Entity holder) {
+            return false;
+        }
+    };
 
     private EntityComponent() {}
 }
