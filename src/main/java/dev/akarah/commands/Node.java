6df1ba000b82098edc165a0f3cf907eb19306445
akarahdev <endisticakarah@gmail.com>
2024-10-01 09:09:19 -0400

    Add node records

--- src/main/java/dev/akarah/commands/Node.java
+++ src/main/java/dev/akarah/commands/Node.java
@@ -1,23 +1,42 @@
 package dev.akarah.commands;
 
-public interface Node {
-    static Node group(Node... nodes) {
+public sealed interface Node {
+    record Group(Node[] nodes) implements Node {
+        
+    }
+    record Literal(String content) implements Node {
 
+    }
+
+    record Argument(String name, ParameterType parameterType) implements Node {
+
+    }
+
+    record Optional(Node child) implements Node {
+
+    }
+
+    record Execute(Runnable task) implements Node {
+
+    }
+
+    static Node group(Node... nodes) {
+        return new Group(nodes);
     }
 
     static Node literal(String content) {
-
+        return new Literal(content);
     }
 
     static Node argument(String name, ParameterType parameterType) {
-
+        return new Argument(name, parameterType);
     }
 
     static Node optional(Node child) {
-
+        return new Optional(child);
     }
 
     static Node execute(Runnable runnable) {
-
+        return new Execute(runnable);
     }
 }
